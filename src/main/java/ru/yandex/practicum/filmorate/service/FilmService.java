e553bc6bfb01ccafd602e3f10f3caee83f0b7adb
Sergey Ryadovoy <ryadovoysergey@gmail.com>
2023-06-08 16:27:40 +0600

    sprint 10, 1 review v1.01

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
