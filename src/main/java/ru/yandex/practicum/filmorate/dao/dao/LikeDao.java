a9d8e0c7c94504ea74ad94f062ccbdab3ead6691
Sergey Ryadovoy <ryadovoysergey@gmail.com>
2023-07-14 13:19:01 +0600

    Sprint 11 v1.2

--- src/main/java/ru/yandex/practicum/filmorate/dao/dao/LikeDao.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/dao/LikeDao.java
@@ -1,7 +1,5 @@
 package ru.yandex.practicum.filmorate.dao.dao;
 
-import org.springframework.data.relational.core.sql.In;
-
 public interface LikeDao {
     void addLike(Integer filmId, Integer userId);
 
